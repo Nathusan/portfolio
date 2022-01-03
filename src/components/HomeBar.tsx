8d09235ec7f94b9567b6de89b4eac4e724b576ca
Nathusan <nithusan.sivalinga@wrenkitchens.com>
2022-01-03 11:29:34 +0000

    P-1 adding toggle button

--- src/components/HomeBar.tsx
+++ src/components/HomeBar.tsx
@@ -3,11 +3,18 @@ import { Link } from "react-router-dom";
 import '../style/homeBar.less';
 
 export function Homebar(): JSX.Element {
+    const onClick = () => {
+        const navBar = document.getElementsByClassName('navbar-links')[0];
+        const toggleButton = document.getElementsByClassName('toggle-button')[0];
+
+        navBar.classList.toggle('active')
+    }
+
     return (
         <div className="home-bar">
             <nav>
             <div className="nith-title">Nith's Portfolio</div>
-            <a className="toggle-button">
+            <a className="toggle-button" onClick={onClick}>
                 <span className="bar"></span>
                 <span className="bar"></span>
                 <span className="bar"></span>
